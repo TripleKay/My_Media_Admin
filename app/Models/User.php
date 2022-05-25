6aa590f8545b9852ad868bac5f7210c8f78bb379
TripleKay <triplekay.k3@gmail.com>
2022-05-25 18:03:07 +0630

    database

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'user_id',
         'name',
         'email',
+        'phone',
+        'address',
+        'gender',
         'password',
     ];
 
